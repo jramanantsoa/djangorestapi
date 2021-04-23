293acc2d6a0fd5811ed2fdf23f7b1429cef56c25
Jessica RAMANANTSOA <fifaliana.dev@gmail.com>
2021-04-23 17:38:03 +0200

    heroku

--- apirest/apirest/settings.py
+++ apirest/apirest/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for apirest project.
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
